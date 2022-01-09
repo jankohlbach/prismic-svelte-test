d1b00746d4b05eecb5749f487f8057d3479757e2
Jan Kohlbach <mail@jankohlbach.dev>
2022-01-09 16:49:26 +0100

    🚸 add prismic preview feature

---
+++ src/routes/preview.svelte
@@ -0,0 +1,14 @@
+<script>
+  import createClient from "$lib/prismicClient"
+  import { goto } from "$app/navigation"
+  import { onMount } from "svelte"
+
+  onMount(async () => {
+    const client = createClient()
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get("token")
+    const documentId = urlParams.get("documentId")
+    const redirect = await client.resolvePreviewURL({defaultURL: "/"})
+    await goto(redirect) // This does a client-side redirect to that URL
+  })
+</script>
